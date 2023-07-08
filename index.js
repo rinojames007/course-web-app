f9ff657e004a81a9465e2fc58223ea950238e963
rino james <rinojames.krl@gmail.com>
2023-07-08 09:36:21 +0530

    complete basic API

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 const port = 3000;
 
 // middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json())
+app.use(express.json());
 
 // secrets and connection uri
 const URI = process.env.connectionStr;
@@ -58,6 +58,22 @@ const adminAuthentication = (req, res, next) => {
     } else {
         res.sendStatus(401);
     }
+};
+
+const userAuthentication = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(authHeader){
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, userSecret, (err, user) => {
+            if(err) {
+                res.sendStatus(403)
+            }
+            req.user = user;
+            next();
+        })
+    } else {
+        res.sendStatus(401);
+    }
 }
 
 // admin routes
@@ -92,10 +108,13 @@ app.post('/admin/course/add', adminAuthentication, async(req, res) => {
     res.json({ message: "course added successfully" })
 });
 
-app.put('/admin/course/edit', adminAuthentication, async(req, res) => {
-    const { title, description, price, imageLink, published, _id } = req.body;
-    await Course.findOneAndUpdate({ _id: _id }, { title: title, description: description, price: price, imageLink: imageLink, published: published })
-    res.json({ message: "course updated successfully" })
+app.put('/admin/course/:courseId', adminAuthentication, async(req, res) => {
+    const course = await Course.findByIdAndUpdate(req.params.courseId, req.body, { new: true });
+    if(course) {
+        res.json({ message: "course updated successfully" })
+    } else {
+        res.status(404).json({ message: "course not found"})
+    }
 });
 
 app.delete('/admin/course/edit', adminAuthentication, async (req, res) => {
@@ -104,13 +123,66 @@ app.delete('/admin/course/edit', adminAuthentication, async (req, res) => {
     res.json({ message: "course deleted successfully "})
 })
 // redundant
+// app.get('/admin/course/view', async (req, res) => {
+//     const course = await Course.find()
+//     res.json({ course })
+// })
 
-app.get('/admin/course/view', async (req, res) => {
-    const course = await Course.find()
-    res.json({ course })
+// user routes
+app.post('/user/signup', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if(user) {
+        res.status(403).json({ message: "user already exists"})
+    } else {
+        const newUser = new User({ username, password })
+        await newUser.save();
+        const token = jwt.sign({ username }, userSecret, { expiresIn: '1h' })
+        res.json({ message: "user created successfully", token});
+    }
+});
+
+app.post('/user/login', async (req, res) => {
+  const { username, password } = req.body;
+  const findUser = await User.findOne({ username, password });
+  if(findUser){
+      const token = jwt.sign({ username }, userSecret, { expiresIn: '1h'});
+      res.json({ message: "logged in successfully", token })
+  } else {
+      res.status(403).json({ message: "invalid credentials", token })
+  }
+});
+
+app.get('/user/course', userAuthentication, async (req, res) => {
+    const allCourses = await Course.find({ published: true });
+    res.json({ allCourses });
 })
 
+app.post('/user/course/:courseId', userAuthentication, async (req, res) => {
+    const courseId = req.params.courseId;
+    const course = await Course.findById(courseId);
+    if(course) {
+        const user = await User.findOne({ username: req.user.username });
+        if(user) {
+            user.purchasedCourses.push(course);
+            await user.save();
+            res.json({ message: "course purchased successfully" });
+        } else {
+            res.status(404).json({ message: "user not found"})
+        }
+    } else {
+        res.status(404).json({ message: "course not found" })
+    }
+})
 
+app.get('/user/course/purchased', userAuthentication, async (req, res) => {
+    const user = await User.findOne({ username: req.user.username }).populate('purchasedCourses');
+    if(user){
+        res.json({ purchasedCourses: user.purchasedCourses || [] });
+    } else {
+        res.status(403).json({ message: "user not found" });
+    }
+});
 
 // connecting to database
 mongoose.connect(URI);
